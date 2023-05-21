573de3c669d95cd8ed0733db58762338ff1cf4ec
Pharez <Staygold93@github.com>
2023-05-20 19:29:59 -0500

    added navbar comp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom' ;
 import { Box } from '@mui/material';
 
+import  {Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed} from './components';
+
 const App = () => (
   
 <BrowserRouter>
@@ -17,5 +19,6 @@ const App = () => (
 </BrowserRouter>
   
 );
+
 
 export default App
